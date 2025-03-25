452f16c4fa1bd07fa2ba19f566bc79502d0992c4
Abdur Rehman <abdur.rehman@hatio.in>
2025-03-25 09:40:36 +0530

    commit

---
+++ MySDKSPM/Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MySDKSPM",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(
+            name: "MySDKSPM",
+            targets: ["MySDKSPM"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/elai950/AlertToast", from: "1.3.9")
+    ],
+    targets: [
+        .target(
+            name: "MySDKSPM",
+            dependencies: [
+                .target(name: "MySDK"),
+                .product(name: "AlertToast", package: "AlertToast")
+            ]
+        ),
+        .testTarget(
+            name: "MySDKSPMTests",
+            dependencies: ["MySDKSPM"]
+        ),
+        .binaryTarget(
+            name: "MySDK",
+            path: ".Sources/MySDK.xcframework"
+        )
+    ]
+)
